bff0a176a45c7bad558f3e49f338b2a3ab3cba0c
smm <2080814813@qq.com>
2023-12-13 17:02:50 +0800

    tryings

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
 
 //精准定位错误
-// const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   //设置正向代理服务器
   devServer:{
     proxy:"http://localhost:3000"
   },
-  // devtool: "source-map",
-  // plugins: [
-  //   sentryWebpackPlugin({
-  //     //组织名称
-  //     org: 'houpu',
-  //     //项目名称
-  //     project: "wyy",
-  //     //密钥
-  //     authToken: sntrys_eyJpYXQiOjE3MDI0NDgxOTcuOTMyODU1LCJ1cmwiOiJodHRwczovL3NlbnRyeS5pbyIsInJlZ2lvbl91cmwiOiJodHRwczovL3VzLnNlbnRyeS5pbyIsIm9yZyI6ImhvdXB1In0 = _EE0+LuJ807p3biyZVUM1CQlt6zFGWftF5obtElm3Jk8,
-  //   }),
-  // ],
+  configureWebpack:{
+    devtool: "source-map",
+    plugins: [
+      sentryWebpackPlugin({
+        //组织名称
+        org: 'houpu',
+        //项目名称
+        project: "wyy",
+        //密钥
+        authToken: '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'
+      }),
+    ],
+  }
+  
 })
